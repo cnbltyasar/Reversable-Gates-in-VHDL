9520325921e89c5e771c7d70eb8bc2d5ca78657d
Yaşar Canbolat <cnbltyasar@gmail.com>
2025-07-27 22:43:40 +0300

    Reversable gate VHDL files added TSG Gate updated

--- tsg_gate.vhd
+++ tsg_gate.vhd
@@ -1,43 +1,20 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 27.07.2025 22:16:37
--- Design Name: 
--- Module Name: tsg_gate - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
 entity tsg_gate is
---  Port ( );
+    Port ( A : in STD_LOGIC;
+           B : in STD_LOGIC;
+           Cin : in STD_LOGIC;
+           P : out STD_LOGIC;
+           Q : out STD_LOGIC;
+           Sum : out STD_LOGIC;
+           Cout : out STD_LOGIC );
 end tsg_gate;
 
 architecture Behavioral of tsg_gate is
-
 begin
-
-
+    P <= A;
+    Q <= B;
+    Sum <= A xor B xor Cin;
+    Cout <= (A and B) or (B and Cin) or (A and Cin);
 end Behavioral;
